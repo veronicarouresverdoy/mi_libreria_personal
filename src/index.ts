b200850e94ff9fdaf6314ddb999d77665bf80c62
Jano Soler <janosolerdiaz@gmail.com>
2024-10-16 12:50:52 +0200

    feat: configurado servidor de archivos estáticos

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import Express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = Express();
 const port = 3000;
 
-
+const publicPath = path.join(__dirname, '../public');
+app.use(Express.static(publicPath));
 
 app.get('/', (req: Express.Request, res: Express.Response) => {
-  res.send('<h1>Hello World!</h1>');
-})
+  const targetFilePath = path.join(publicPath, "/index.html");
+  res.sendFile(targetFilePath);
+});
 
 
 app.listen(port, () => {
