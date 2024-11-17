6b83032a1de245e2d0c4ce4fb285c8f92f2711f8
Herena <herenaojeda@gmail.com>
2024-11-17 18:26:25 +0100

    cambios book

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,6 +2,7 @@ import Express from 'express';
 import { publicPath } from './config/configData.js';
 import { staticRouter } from './routes/staticRouter.js';
 import apiRouter from './routes/apiRouter.js';
+import bookRouter from './routes/bookRouter.js';
 
 const app = Express();
 const port = 3000;
@@ -12,6 +13,7 @@ app.use(Express.static(publicPath));
 
 app.use("/", staticRouter);
 app.use("/api/v1/", apiRouter);
+app.use("/api/v1/books", bookRouter);
 
 
 app.listen(port, () => {
--- src/backend/routes/apiRouter.ts
+++ src/backend/routes/apiRouter.ts
@@ -1,10 +1,12 @@
 import Express from 'express';
 import userRouter from './userRouter.js';
+import bookRouter from './bookRouter.js';
 
 
 const apiRouter = Express.Router();
 
 apiRouter.use("/users", userRouter);
+apiRouter.use("/books", bookRouter);
 
 
 export default apiRouter;
